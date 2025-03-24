3d54655650458f1385c6a7af852e4dc9cc9b7ea4
mythunlearner <learntechpassion@gmail.com>
2025-03-24 12:52:51 +0530

    Added Login component

--- README.md
+++ README.md
@@ -5,3 +5,18 @@
 - Install Tailwind css
 - Install Daisy UI
 - Add NavBar Component to App.jsx
+- Create a NavBar as a seperate Component file
+- Installed react router
+- Create BrowserRouter > Routes > Route=/ Body > RouteChildren
+- Create an Outlet in your Body Component
+- Created a Footer
+
+
+
+
+Body
+  NavBar
+  Route=/ => Feed
+  Route=/login => Login
+  Route=/conections => Connections
+  Route=/profile => profile
---
+++ src/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex justify-center my-10'>
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div>
+
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login
